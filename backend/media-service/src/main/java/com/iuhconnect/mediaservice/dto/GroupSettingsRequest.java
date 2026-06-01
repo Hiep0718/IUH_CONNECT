67740124996b110dfb13288aea364745c01eda01
phihungdeptraino2 <hungdz1372004@gmail.com>
2026-06-01 22:25:01 +0700

    fix: clean up unused files in media-service and conversation-service

--- backend/media-service/src/main/java/com/iuhconnect/mediaservice/dto/GroupSettingsRequest.java
+++
@@ -1,9 +0,0 @@
-package com.iuhconnect.mediaservice.dto;
-
-import lombok.Data;
-
-@Data
-public class GroupSettingsRequest {
-    private Boolean requireApproval;
-    private Boolean allowMemberInvite;
-}
